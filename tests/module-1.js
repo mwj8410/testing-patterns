160cc7369910b4a2e674b107ecbc74c12faf6bf1
Matthew Jackson <mwj8410@gmail.com>
2018-06-15 08:47:42 -0500

    Initial test pattern Includes tagged tests to allow for running subsets of the test suite

---
+++ tests/module-1.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const {expect} = require('chai')
+
+describe('First Module', () => {
+
+  describe('Top Section', () => {
+    it('does a critical thing. @critical @smoke', () => {
+      expect(1).equal(1)
+    })
+
+    it('does a smoke thing. @smoke', () => {
+      expect(1).equal(1)
+    })
+
+    it('does a regression thing. @regression', () => {
+      expect(1).equal(1)
+    })
+  })
+
+  describe('Body Section', () => {
+    it('does a critical thing. @critical @smoke', () => {
+      expect(1).equal(1)
+    })
+
+    it('does a smoke thing. @smoke', () => {
+      expect(1).equal(1)
+    })
+
+    it('does a regression thing. @regression', () => {
+      expect(1).equal(1)
+    })
+  })
+
+  describe('Footer Section', () => {
+    it('does a critical thing. @critical @smoke', () => {
+      expect(1).equal(1)
+    })
+
+    it('does a smoke thing. @smoke', () => {
+      expect(1).equal(1)
+    })
+
+    it('does a regression thing. @regression', () => {
+      expect(1).equal(1)
+    })
+  })
+
+})
